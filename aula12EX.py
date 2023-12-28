0521be40d42d290b169e3fd889af3c2cde125cca
Gustavo Costa <gustavo25costa25@gmail.com>
2023-12-28 20:43:05 -0300

    exercico aula12 completo

---
+++ aula12EX.py
@@ -0,0 +1,7 @@
+nome = "Gustavo da Costa Santos"
+altura = 1.74
+peso = 72.8
+imc = peso / (altura**2)
+
+print(nome + " tem " + str(altura) + " de altura")
+print("pesa " + str(peso) + " quilos e sue IMC é de " + str(imc))
