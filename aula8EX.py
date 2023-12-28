e13755eb9cee2566cfb2c0ce9b26e60d9af40b85
Gustavo Costa <gustavo25costa25@gmail.com>
2023-12-28 20:41:52 -0300

    exercicio aula8 completo

---
+++ aula8EX.py
@@ -0,0 +1,13 @@
+nome = 'Gustavo' #str
+sobrenome = 'Santos' #str
+idade = 26 #int
+ano_nascimento = 2023 - idade #int
+maior_de_idade = idade >= 18 #bool
+altura_metros = 1.74 #float 
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print("Ano de nascimento:", ano_nascimento)
+print('É maior de idade:?', maior_de_idade)
+print('Altura em metros:', altura_metros)
