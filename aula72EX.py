263e599ef533997c46943dc2ad0def7b056ed336
Gustavo Costa <gustavo25costa25@gmail.com>
2024-01-02 16:41:14 -0300

    initial exercise of funtions

---
+++ aula72EX.py
@@ -0,0 +1,25 @@
+def multiplicar(*args):
+    total = 1
+    for n in args:
+        total *= n
+    return total
+
+
+multiplicação = multiplicar(10, 2, 3, 4, 5)
+print(multiplicação)
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def even_or_odd(x):
+    if x%2 == 0:
+        return print(f'{x} is Even')
+    else:
+        return print(f'{x} is Odd')
+    
+value_x = input('Enter an integer value: ')
+try:
+    value_x = int(value_x)
+    even_or_odd(value_x)
+except ValueError:
+    print('Value entered is not integer!')
