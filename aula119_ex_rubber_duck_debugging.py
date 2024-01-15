2e29f9ddf12e26770bbb26974a3224679242641b
Gustavo Costa <gustavo25costa25@gmail.com>
2024-01-15 20:04:42 -0300

    function in dic

--- aula119_ex_rubber_duck_debugging.py
+++ aula119_ex_rubber_duck_debugging.py
@@ -2,64 +2,74 @@
 # Música para codar =)
 # Everybody wants to rule the world - Tears for fears
 
-import os
-import sys
-import json
-print('---------- Bem vindo a sua lista de tarefas 📝 -------------')
-
-list_everything = []
-list_remake = []
+import os, sys, json
+print('---------- Bem vindo a sua lista de tarefas📝 -------------')
 
 def validate_option(option):
     if option:
-        if option in ('LISTAR', 'DESFAZER', 'REFAZER','SAIR'):
+        if option in ('INSERIR', 'DESFAZER', 'REFAZER','SAIR', 'LISTA'):
             return option
         else:
-            os.system('cls')
-            print('Opção invalida    ⚠️ ')
+            print('Opção invalida ⚠️\n')
     else:
-        os.system('cls')
-        print('🚫 Por favor, informe uma opção  🚫')
+        print('🚫 Por favor, informe uma opção 🚫\n')
 
-def return_option(option,iten = None):
-    if option == 'LISTAR':
-        os.system('cls')
-        iten = input('\n✍🏾 Informe a inclusão: ')
-        item_include = iten 
-        list_everything.append(item_include)
+def include_option(iten = None):
+    iten = input('\n✍🏾 Informe a tarefa: ')
+    if iten is not None and iten is not '':
+        list_everything.append(iten)
+        print(f'Item {iten} incluso com sucesso ✅\n')
+    else:
+        print('Nenhum item informado ⚠️\n')
     
-    elif option == 'DESFAZER':
-        if list_everything == []:
-            os.system('cls')
-            print('Nenhum item para ser removido    ⚠️ ')
-        else:
-            list_remake.append(list_everything.pop()) 
+def remove_option():
+    if list_everything == []:
+        print('Nenhum item para ser removido ⚠️ \n')
+    else:
+        list_remake.append(list_everything.pop()) 
+        print(f'Item {list_remake[-1]} removido com sucesso ✅\n')
     
-    elif option == 'REFAZER':
-        if list_remake == []:
-            os.system('cls')
-            print('Nenhum item para ser reposto    ⚠️')
-        else:
-            list_everything.append(list_remake.pop())
+def remake_option():
+    if list_remake == []:
+        print('Nenhum item para ser reposto ⚠️\n')
+    else:
+        list_everything.append(list_remake.pop())
+        print(f'Item {list_everything[-1]} reposto com sucesso ✅\n')
     
-    elif option == 'SAIR':
-        os.system('cls')
-        with open('C:\\Users\\gusta\\Downloads\\list_everything.json','w+',encoding='utf8') as file:
-            json.dump(
-                list_everything,
-                file,
-                ensure_ascii=False,
-                indent=2,
-            )
-        print('💾 Lista de tarefas salva')
-        sys.exit()
+def exit_option():
+    with open('C:\\Users\\gusta\\Downloads\\list_everything.json','w',encoding='utf8') as file:
+        json.dump(
+            list_everything,
+            file,
+            ensure_ascii=False,
+            indent=2,
+        )
+    print("LISTA:",*list_everything, sep='\n')
+    print('\n💾 Lista de tarefas salva\n')
+    sys.exit()
+
+def filer_list():
+    try:
+        with open('C:\\Users\\gusta\\Downloads\\list_everything.json','r',encoding='utf8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+list_everything = filer_list()
+list_remake = []
 
 while True:
     selected_option = validate_option(
-        input('Opções:\n🖊️    Listar\n↩️    Desfazer\n🔄️   Refazer\n❌   Sair\nSelecione uma opção: ').upper().strip()
-    )
-
-    return_option(selected_option)
-    print('\n📝 Lista:')
-    print(*list_everything,sep='\n')
-
+        input('\nOpções:\n🖊️    Inserir\n↩️    Desfazer\n🔄️   Refazer\n\
+🗒️    Lista\n❌   Sair\nSelecione uma opção: ').upper().strip()
+    )   
+    comandos = {
+        "INSERIR": lambda: include_option(),
+        "DESFAZER": lambda: remove_option(),
+        "REFAZER": lambda: remake_option(),
+        "SAIR": lambda: exit_option(),
+        "CLEAR": lambda: os.system('clear'),
+        "LISTA": lambda: print("LISTA:",*list_everything, sep='\n')
+    }
+    comando = (comandos.get(selected_option) if comandos.get(selected_option) is not None else comandos["LISTA"])
+    comando()
