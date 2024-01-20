986e0148c7792eb50e6775f826a11746099d5e2b
Gustavo Costa <gustavo25costa25@gmail.com>
2024-01-20 19:57:39 -0300

    call files and __main__ = '__main__'

---
+++ aula127_ex_class_json_b.py
@@ -0,0 +1,14 @@
+﻿import json
+from aula127_ex_class_json_a import file_json
+
+with open(file_json, 'r', encoding='utf-8') as file:
+    dados_class = json.load(file)
+
+class Time:
+    def __init__(self, **kwargs):
+        for dado in dados_class.keys():
+            setattr(self, dado, kwargs.get(dado, None))
+
+palmeiras = Time(nome = 'Palmeiras', estado = 'São Paulo', estadio = 'Alians Park', torcida = '5 milhões', cores = ['Verde', 'Branco'])
+
+print(palmeiras.__dict__,sep='\n')
---
+++ aula127_ex_class_json_c.py
@@ -0,0 +1,9 @@
+﻿import json
+from aula127_ex_class_json_a import file_json, Time
+
+with open(file_json, 'r', encoding='utf-8') as file:
+    times = json.load(file)
+
+internacinal = Time(nome = 'Internacinal', estado = 'Rio Grande do Sul', estadio = 'Beira Rio', torcida = '2 milhões', cores = ['Vermelho', 'Branco'])
+
+print(internacinal.descricao())
