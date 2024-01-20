dd779e545f1c91ac2b94fae40fec06e0da2d59c7
Gustavo Costa <gustavo25costa25@gmail.com>
2024-01-20 19:55:35 -0300

    call files and __main__ = '__main__'

---
+++ aula127_ex_class_json_a.py
@@ -0,0 +1,30 @@
+﻿import json
+file_json = 'dados_class.json'
+class Time:
+    def __init__(self, nome, estado, estadio, torcida, cores):
+        self.nome = nome
+        self.estado = estado
+        self.estadio = estadio
+        self.torcida = torcida
+        self.cores = cores
+
+    def descricao(self):
+        cores_str = ', '.join(self.cores)
+        return f'---------------------------------------------------------------------------------------------\n'\
+               f'O time {self.nome}, é do Estado do {self.estado},\ntem como casa o estádio {self.estadio} e\n'\
+               f'possui uma torcida de aproximadamente {self.torcida}.\nSuas cores principais são {cores_str}.\n'\
+               f'---------------------------------------------------------------------------------------------\n'
+
+if __name__ == '__main__':
+    vasco = Time('Vasco', 'Rio de Janeiro', 'São Januário', '4,5 milhões', ['Branco', 'Preto', 'Vermelho'])
+
+    print(vasco.descricao())
+
+    with open(file_json,'w',encoding='utf-8') as file:
+        dados = vasco.__dict__
+        json.dump(
+            vasco.__dict__,
+            file,
+            ensure_ascii=False,
+            indent=2
+        )
