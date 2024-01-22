25a11a0742a5912c49d19db82b47b46b0b6a42d6
Gustavo Costa <gustavo25costa25@gmail.com>
2024-01-22 19:11:10 -0300

    correção get

--- aula127_ex_class_json_a.py
+++ aula127_ex_class_json_a.py
@@ -1,5 +1,7 @@
 ﻿import json
 file_json = 'dados_class.json'
+
+
 class Time:
     def __init__(self, nome, estado, estadio, torcida, cores):
         self.nome = nome
@@ -11,16 +13,18 @@ class Time:
     def descricao(self):
         cores_str = ', '.join(self.cores)
         return f'---------------------------------------------------------------------------------------------\n'\
-               f'O time {self.nome}, é do Estado do {self.estado},\ntem como casa o estádio {self.estadio} e\n'\
-               f'possui uma torcida de aproximadamente {self.torcida}.\nSuas cores principais são {cores_str}.\n'\
-               f'---------------------------------------------------------------------------------------------\n'
+            f'O time {self.nome}, é do Estado do {self.estado},\ntem como casa o estádio {self.estadio} e\n'\
+            f'possui uma torcida de aproximadamente {self.torcida}.\nSuas cores principais são {cores_str}.\n'\
+            f'---------------------------------------------------------------------------------------------\n'
+
 
 if __name__ == '__main__':
-    vasco = Time('Vasco', 'Rio de Janeiro', 'São Januário', '4,5 milhões', ['Branco', 'Preto', 'Vermelho'])
+    vasco = Time('Vasco', 'Rio de Janeiro', 'São Januário',
+                 '4,5 milhões', ['Branco', 'Preto', 'Vermelho'])
 
     print(vasco.descricao())
 
-    with open(file_json,'w',encoding='utf-8') as file:
+    with open(file_json, 'w', encoding='utf-8') as file:
         dados = vasco.__dict__
         json.dump(
             vasco.__dict__,
--- aula127_ex_class_json_b.py
+++ aula127_ex_class_json_b.py
@@ -4,27 +4,27 @@ from aula127_ex_class_json_a import file_json
 with open(file_json, 'r', encoding='utf-8') as file:
     dados_class = json.load(file)
 
+
 class Time:
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         for dado in dados_class.keys():
-            setattr(self, dado, kwargs.get(dado,None))
+            setattr(self, dado, kwargs.get(dado, None))
+            setattr(self.__class__, dado, property(
+                self.getter(dado), self.setter(dado)))
 
-        for dado in dados_class.keys():
-            @property
-            def dado(self):
-                return self.dado
+    def getter(self, name):
+        def get(self):
+            return getattr(self, name)
+        return get
 
-            @dado.setter
-            def dado(self, valor):
-                self.dado = valor
+    def setter(self, name):
+        def set(self, value):
+            setattr(self, name, value)
+        return set
 
 
-palmeiras = Time(nome = 'Palmeiras', estado = 'São Paulo', estadio = 'Alians Park', torcida = '5 milhões', cores = ['Verde', 'Branco'])
+palmeiras = Time(nome='Palmeiras', estado='São Paulo',
+                 estadio='Alians Park', torcida='5 milhões', cores=['Verde', 'Branco'])
 
-palmeiras.nome = 'Palmeiras'
-palmeiras.estado = 'São paulo'
-palmeiras.estadio = 'Allianz Park'
-palmeiras.torcida = '5 milhões'
-palmeiras.cores = ['Verde','Branco']
 
 print(palmeiras.__dict__)
